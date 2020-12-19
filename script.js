de1fd4d6945770848d631f7989021e2c4a5388eb
saddamarbaa <saddamarbaas@gmail.com>
2020-12-19 18:55:02 +0700

    Responsive Image Gallery With CSS Grid

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 const overlay = document.querySelector(".overlay");
 const galleryImages = document.querySelectorAll(".gallery__image");
+const overlayClose = document.querySelector(".close");
 
 const handleClick = (event) => {
-  // get src of clicked image
+  // get src of the clicked image
   // const src = event.target.src;
   const src = event.currentTarget.querySelector("img").src;
+  // console.log(src);
 
   const overlayImage = overlay.querySelector("img");
   overlayImage.src = src;
 
   // open overlay
   overlay.classList.add("open");
-
   // console.log(overlayImage.src);
 };
 
@@ -21,5 +22,5 @@ const closeImage = () => {
 
 galleryImages.forEach((galleryImage) => {
   galleryImage.addEventListener("click", handleClick);
-  overlay.addEventListener("click", closeImage);
+  overlayClose.addEventListener("click", closeImage);
 });
